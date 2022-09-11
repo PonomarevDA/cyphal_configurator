989b19bcbcd875b4f1c72acd02a4bb8bf609787b
Dmitry Ponomarev <ponomarevda96@gmail.com>
2022-09-11 17:27:58 +0300

    clean up setup scripts

--- scripts/setup_ardupilot_hitl.sh
+++ scripts/setup_ardupilot_hitl.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source $SCRIPT_DIR/setup_base.sh
 
-# User config
-REPOSITORY_PATH="/catkin_ws/src/cyphal_tools"
-REG_DATA_TYPE_PATH="$REPOSITORY_PATH/public_regulated_data_types/uavcan $REPOSITORY_PATH/public_regulated_data_types/reg/"
 CONFIG_LOCAL_PATH="config/ardupilot_hitl_registers.yaml"
+cat $SCRIPT_DIR/../$CONFIG_LOCAL_PATH | y rb
 
-# Common config
-export YAKUT_COMPILE_OUTPUT="$REPOSITORY_PATH/compile_output"
-export YAKUT_PATH="$YAKUT_COMPILE_OUTPUT"
-export UAVCAN__CAN__IFACE='socketcan:slcan0'
-export UAVCAN__CAN__MTU=8
-export UAVCAN__NODE__ID=127
-
-
-# Configure
-yakut compile $REG_DATA_TYPE_PATH -O $YAKUT_COMPILE_OUTPUT
-/catkin_ws/src/cyphal_tools/scripts/create_slcan_from_serial.sh $CYPHAL_DEV_PATH_SYMLINK
-cat $REPOSITORY_PATH/$CONFIG_LOCAL_PATH | y rb
 y cmd 42 restart -e
--- scripts/setup_base.sh
+++ scripts/setup_base.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-
-# User config
-REPOSITORY_PATH="/catkin_ws/src/cyphal_tools"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+REPOSITORY_PATH="$SCRIPT_DIR/.."
 REG_DATA_TYPE_PATH="$REPOSITORY_PATH/public_regulated_data_types/uavcan $REPOSITORY_PATH/public_regulated_data_types/reg/"
-
-# Common config
 export YAKUT_COMPILE_OUTPUT="$REPOSITORY_PATH/compile_output"
 export YAKUT_PATH="$YAKUT_COMPILE_OUTPUT"
 export UAVCAN__CAN__IFACE='socketcan:slcan0'
 export UAVCAN__CAN__MTU=8
 export UAVCAN__NODE__ID=127
-
-# Configure
 yakut compile $REG_DATA_TYPE_PATH -O $YAKUT_COMPILE_OUTPUT
 /catkin_ws/src/cyphal_tools/scripts/create_slcan_from_serial.sh $CYPHAL_DEV_PATH_SYMLINK
