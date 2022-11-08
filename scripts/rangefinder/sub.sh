1b9948298d248cd252b0428e05a01eb89aa77f5d
Dmitry Ponomarev <ponomarevda96@gmail.com>
2022-11-09 00:32:43 +0300

    add rangefidner sub script

---
+++ scripts/rangefinder/sub.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+print_help () {
+  echo "Usage: servo/sub.sh <node_id>"
+  exit
+}
+
+NODE_ID=$1
+if [ -z "$NODE_ID" ]; then
+    print_help
+fi
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/..
+$SCRIPT_DIR/sub.sh $NODE_ID uavcan.pub.range
---
+++ scripts/setup_ardupilot_and_2_micro.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source $SCRIPT_DIR/base.sh
+
+CONFIG_LOCAL_PATH="config/ardupilot_and_2_micro.yaml"
+cat $SCRIPT_DIR/../$CONFIG_LOCAL_PATH | y rb
+
+y cmd 42 restart -e
+y cmd 50-53 65530 -e
+y cmd 50-53 restart -e
