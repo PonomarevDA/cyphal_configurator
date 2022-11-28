d6dd2bd638bfbf8a4b653729c5c8085b6c253429
Dmitry Ponomarev <ponomarevda96@gmail.com>
2022-11-28 21:09:44 +0300

    improve servo/readiness_pub.sh

--- scripts/servo/readiness_pub.sh
+++ scripts/servo/readiness_pub.sh
@@ -13,13 +13,23 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/
 source $SCRIPT_DIR/init.sh
 
 READINESS_REGISTER_BASE=uavcan.sub.readiness
-READINESS_PORT_ID=$(y r $NODE_ID $READINESS_REGISTER_BASE.id)
-READINESS_DATA_TYPE=$(y r $NODE_ID $READINESS_REGISTER_BASE.type | tr -d '"')
 
-echo Readiness Port id: $READINESS_PORT_ID
-echo Readiness Data type: $READINESS_DATA_TYPE
+for i in {1..5}; do
+  READINESS_PORT_ID=$(y r $NODE_ID $READINESS_REGISTER_BASE.id)
+  if [ "$READINESS_PORT_ID" != "null" ]; then
+    break
+  fi
+done
+
+for i in {1..5}; do
+  READINESS_DATA_TYPE=$(y r $NODE_ID $READINESS_REGISTER_BASE.type | tr -d '"')
+  if [ "$READINESS_DATA_TYPE" != "null" ]; then
+    break
+  fi
+done
+
 if ((READINESS_PORT_ID < 1 || READINESS_PORT_ID > 6143)); then
     exit -1
 fi
-
+echo "y pub -T 0.1" $READINESS_PORT_ID:$READINESS_DATA_TYPE 3
 y pub -T 0.1 $READINESS_PORT_ID:$READINESS_DATA_TYPE 3
